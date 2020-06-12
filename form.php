515b0595ef2f101cbebc7793c0a314abb9471cb0
Oumar Bela Sidibe <63612473+Oumarsidibe0514@users.noreply.github.com>
2020-06-12 10:35:23 -0700

    Update form.php

--- form.php
+++ form.php
@@ -1,13 +1,12 @@
 <?php
-if (){//show data
-
+if (isset($_GET["FirstName"])){//show data
+  echo $_GET["FirstName"];
 }else{//show form
   echo 
   '
   <form action="">
     <p>First Name <input type="text" name="FirstName" /></p>
     <p><input type="Submit" /></p>
-  
   </form>
   
   ';
